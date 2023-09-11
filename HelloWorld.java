fa903064507fd5665660aa9cda7fc9a7f59defd5
Hudson Land <hudson.land@mrjaffesclass.com>
2023-09-11 11:36:17 -0700

    jsinfsefn

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,13 @@
 
-
 public class HelloWorld 
 {
     public static void main(String[] args) 
     {
         System.out.println("Like this?");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+
     }
 }
 
